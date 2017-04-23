c6768ffed157b674d8c5944466b1cbf43dfbe8a7
houdunwang <2300071698@qq.com>
2017-04-23 19:54:58 +0800

    no message

--- src/DbProvider.php
+++ src/DbProvider.php
@@ -7,24 +7,32 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\db;
+
 use houdunwang\framework\build\Provider;
 
 /**
  * Class DbProvider
+ *
  * @package hdphp\db
  */
-class DbProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class DbProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
+    public function boot()
+    {
+    }
 
-	}
-
-	public function register() {
-		$this->app->bind( 'Db', function () {
-			return new Db();
-		} );
-	}
+    public function register()
+    {
+        $this->app->bind(
+            'Db',
+            function () {
+                return new Db();
+            }
+        );
+    }
 }
